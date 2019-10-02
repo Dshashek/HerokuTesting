ed792f56164b74d63286c8832b7ab8897ad68a4c
Derek <dshashek@gmail.com>
2019-10-02 18:24:16 -0500

    removed python pg stuff

--- area51financial/app.py
+++ area51financial/app.py
@@ -1,22 +1,10 @@
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
 from flask import Flask,render_template,request, url_for
-from flask_sqlalchemy import SQLAlchemy
-import sys
-import json
 import pandas as pd
-import psycopg2
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost/area51financial')
-testing = engine.execute("SELECT * FROM history")
-for record in testing:
-    print(record)
 
 @app.route("/")
 def index():

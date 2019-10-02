5531b8a3ea0236f239f986af87f5c5abf4053abd
Derek <dshashek@gmail.com>
2019-10-02 01:06:03 -0500

    updated file path

--- area51financial/app.py
+++ area51financial/app.py
@@ -32,6 +32,6 @@ if __name__ == '__main__':
 # In[5]:
 
 
-df=pd.read_excel('Par_DataforLogisticRegression.xlsx',skiprows=2)
+df=pd.read_excel('./Par_DataforLogisticRegression.xlsx',skiprows=2)
 df['Age'].to_json()
 

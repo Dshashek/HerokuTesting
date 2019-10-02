780f3882239b8a95a7c816d430be898f8884ddc8
Derek <dshashek@gmail.com>
2019-10-02 01:32:27 -0500

    removed bad xls reference

--- area51financial/app.py
+++ area51financial/app.py
@@ -10,28 +10,12 @@ from flask import Flask,render_template,request
 import pandas as pd
 
 app = Flask(__name__)
-
-def r1():
-    df=pd.read_excel('./static/Par_DataforLogisticRegression.xlsx',skiprows=2)
-    return df['Age'].to_json()
-    
+   
 
 @app.route("/")
 def index():
     return render_template('Index.html')
 
-@app.route('/age')
-def age():
-    age=r1()
-    return age
 
 if __name__ == '__main__':
     app.run(debug=False)
-
-
-# In[5]:
-
-
-df=pd.read_excel('./Par_DataforLogisticRegression.xlsx',skiprows=2)
-df['Age'].to_json()
-

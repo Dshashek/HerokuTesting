241722e14a4be8309f0d3daac64ca8f2eec5a0b0
Derek <dshashek@gmail.com>
2019-10-02 01:09:35 -0500

    new app

--- area51financial/app.py
+++ area51financial/app.py
@@ -1,37 +1,17 @@
 
 
-# coding: utf-8
+#import dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 
-# In[1]:
 
-
-from flask import Flask,render_template,request
-
-import pandas as pd
-
-app = Flask(__name__)
-
-def r1():
-    df=pd.read_excel('./static/Par_DataforLogisticRegression.xlsx',skiprows=2)
-    return df['Age'].to_json()
-    
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return render_template('Index.html')
 
-@app.route('/age')
-def age():
-    age=r1()
-    return age
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=False)
-
-
-# In[5]:
-
-
-df=pd.read_excel('./Par_DataforLogisticRegression.xlsx',skiprows=2)
-df['Age'].to_json()
-

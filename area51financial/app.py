da14050e39f9234c1390c883bfe558947172daa6
Derek <dshashek@gmail.com>
2019-10-04 15:13:35 -0500

    indents

--- area51financial/app.py
+++ area51financial/app.py
@@ -41,7 +41,7 @@ def predictor (nn_inputs):
     else:
        print("You are not approved")
 
-    return print('finished'), redirect('/',code=302)
+    return redirect('/',code=302)
 
 app = Flask(__name__)
 

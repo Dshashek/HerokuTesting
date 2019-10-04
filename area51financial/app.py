ceb36a5c7bb79640ea8c3cfbeaa602530f81d192
Derek Shashek <dshashek@gmail.com>
2019-10-03 21:45:10 -0500

    added predictor

--- area51financial/app.py
+++ area51financial/app.py
@@ -4,10 +4,11 @@ import pandas as pd
 import requests
 import os
 import psycopg2
+import tensorflow
 from tensorflow.keras.models import load_model
+import numpy as np
 
 def to_nn_inputs(input):
-
 
     input_options = ['A40','A41','A410','A42','A43','A44','A45','A46','A48','A49','A71','A72','A73','A74','A75','A101','A102','A103','A151','A152','A153','A191','A192','A201','A202','B10','B11','B12','B13','B14','B15','B20','B21','B22','B23','B24','B25','B26','B27','B30','B31','B32','B33','B34','B35']
     pd_proto = {}
@@ -25,7 +26,19 @@ def to_nn_inputs(input):
 
     nn_inputs = df.values.tolist()[0]
 
+    predictor(nn_inputs)
     return print(nn_inputs)
+
+def predictor (nn_inputs):
+   myarray = np.asarray(nn_inputs)
+   model = load_model("area_51_loan_approval.h5")
+   data = np.expand_dims(myarray, axis=0)
+   prediction = model.predict(data).round()
+   prediction.tolist()
+   if prediction[0][0] == 1.0:
+       print("You are approved")
+   else:
+       print("You are not approved")
 
 app = Flask(__name__)
 
